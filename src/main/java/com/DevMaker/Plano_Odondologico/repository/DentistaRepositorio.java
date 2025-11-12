e890e99a48bef5709bf99441368be235013f08c7
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-11-12 10:40:44 -0300

    chore: adicionar o @repository antes da classe

--- src/main/java/com/DevMaker/Plano_Odondologico/repository/DentistaRepositorio.java
+++ src/main/java/com/DevMaker/Plano_Odondologico/repository/DentistaRepositorio.java
@@ -2,9 +2,11 @@ package com.DevMaker.Plano_Odondologico.repository;
 
 import com.DevMaker.Plano_Odondologico.model.Dentista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DentistaRepositorio extends JpaRepository <Dentista, Long>{
     Optional<Dentista> findByCro(String cro);
 }
