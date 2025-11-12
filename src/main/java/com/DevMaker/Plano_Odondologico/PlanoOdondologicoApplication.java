7d045841045e986a3a575e2cc5d5fe8dbe4aff7b
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-11-12 09:49:11 -0300

    primeiro commit

---
+++ src/main/java/com/DevMaker/Plano_Odondologico/PlanoOdondologicoApplication.java
@@ -0,0 +1,13 @@
+package com.DevMaker.Plano_Odondologico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanoOdondologicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanoOdondologicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevMaker/Plano_Odondologico/PlanoOdondologicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevMaker.Plano_Odondologico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanoOdondologicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
