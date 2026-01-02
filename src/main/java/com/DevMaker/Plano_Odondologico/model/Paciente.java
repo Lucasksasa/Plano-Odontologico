7684625ca9f819807774b67040eb1c3ef7fa29ce
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-01-02 11:40:54 -0300

    feat: adicionar a classe Paciente

---
+++ src/main/java/com/DevMaker/Plano_Odondologico/model/Paciente.java
@@ -0,0 +1,32 @@
+package com.DevMaker.Plano_Odondologico.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pacientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private String telefone;
+
+    private String email;
+
+    @Column (name = "data_nascimento")
+    private LocalDate dataNascimento;
+}
