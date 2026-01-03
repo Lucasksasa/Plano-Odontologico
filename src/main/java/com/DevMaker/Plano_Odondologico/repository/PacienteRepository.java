4a01d335699c6a4463ee51d44b3ad336595f73fa
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-01-03 08:18:45 -0300

    feat: adicionar a classe PacienteRepository

---
+++ src/main/java/com/DevMaker/Plano_Odondologico/repository/PacienteRepository.java
@@ -0,0 +1,8 @@
+package com.DevMaker.Plano_Odondologico.repository;
+
+import com.DevMaker.Plano_Odondologico.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+}
