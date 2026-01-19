b96c215771bf954d67ec946ba88ffdd711f6bd90
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-01-19 09:16:27 -0300

    feat: feat: Adiciona validações no DTO

--- src/main/java/com/DevMaker/Plano_Odondologico/dto/AgendarConsultaRequest.java
+++ src/main/java/com/DevMaker/Plano_Odondologico/dto/AgendarConsultaRequest.java
@@ -1,5 +1,7 @@
 package com.DevMaker.Plano_Odondologico.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,12 @@ import java.time.LocalDateTime;
 @Setter
 public class AgendarConsultaRequest {
 
+    @NotNull(message = "O id do paciente é obrigatório")
     private Long pacienteId;
+    @NotNull(message = "O id do dentista é obrigatório")
     private Long dentistaId;
+    @NotNull(message = "A data e hora da consulta são obrigatórias")
+    @Future(message = "A consulta deve ser agendada para uma data futura")
     private LocalDateTime dataHora;
     private String observacoes;
 }
